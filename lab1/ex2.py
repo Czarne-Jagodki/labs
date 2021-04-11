a984d088630c87a92172445b6bd5bcd33e9faa58
MagdalenaGie <magdalenaxgorska@gmail.com>
2021-04-11 23:40:26 +0200

    graph visualization added

--- lab1/ex2.py
+++ lab1/ex2.py
@@ -1,8 +1,37 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
-neighbour_list = {
-          1 : [2, 5],
-          2 : [1, 3, 5],
-          3 : [2, 4],
-          4 : [3, 5],
-          5 : [1, 2, 4]
-}
+array = np.matrix([
+                 [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+                 [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+                 [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+                 [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                 [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+                 [0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+])
+
+def calculate_nodes_position(n_of_nodes):
+    nodes = {}
+    for n in range(0, n_of_nodes):
+        x = math.cos(2 * math.pi / n_of_nodes * n)
+        y = math.sin(2 * math.pi / n_of_nodes * n)
+        nodes.update({n: (x, y)})
+    return nodes
+
+def create_graph_visualization(matrix):
+    nodes = calculate_nodes_position(len(matrix))
+    plt.figure(figsize=(5, 5))
+    graph_visualization = nx.from_numpy_matrix(matrix)
+    nx.draw_networkx(graph_visualization, nodes)
+    plt.show()
+
+#test from example from kotfica's pdf
+create_graph_visualization(array)
