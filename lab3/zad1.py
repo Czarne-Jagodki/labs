46e9918044c299597fabffc6ef36eec1ef948827
JJ807 <jakubstrugaa@gmail.com>
2021-04-29 04:12:54 +0200

    zad1 lab3

---
+++ lab3/zad1.py
@@ -0,0 +1,108 @@
+from itertools import combinations
+from random import sample, randint
+import matplotlib.pyplot as plt
+import networkx as nx
+from labs.structures.Graph import Graph
+from labs.structures.Vertex import Vertex
+
+
+def create_rand_consistent_weighted_graph(n: int = 5, weight_min: int = 1, weight_max: int = 10) -> Graph:
+    """
+    Funkcja tworzaca spojny graf losowy o zadanej liczbie wierczholkow i krawedziach o wadze pomiedzy
+    weight_min a weight_max
+    :param n: liczba wierzcholkow
+    :param weight_min: waga min
+    :param weight_max: waga max
+    :return: gotowy graf
+    """
+
+    vertices = set([v for v in range(n)])  # zbior numerow wierzcholkow
+    edges = set()  # zbior krawedzi (tupli o 3 polach) zlozonych z 2 wierczholkow i losowej wagi
+
+    min_edges = n - 1  # minimalna liczba krawedzi
+    max_edges = (n * (n - 1)) / 2  # maksymalna liczba krawedzi
+
+    # losujemy liczbe krawedzi z przedzialu <min, max> (wlacznie)
+    nr_of_edges = randint(min_edges, max_edges)
+
+    while True:
+        # stworzenie wszystkich mozliwych 2-elementowych kombinacji wierzcholkow w postaci tupli
+        vertex_comb = combinations(vertices, 2)
+
+        # losowe wybranie kombinacji 2 wierzcholkow z listy vertex_comb, 'nr_of_edges' razy, czyli dla kazdej krawedzi
+        rand_comb = sample(list(vertex_comb), nr_of_edges)
+
+        # dodajemy do zbioru 'edges', tuple z wierzcholkami i waga
+        for comb in rand_comb:
+            edges.add((Vertex(comb[0]),
+                       Vertex(comb[1]),
+                       randint(weight_min, weight_max)))
+
+        # tworzymy pusty graf wazony
+        graph = Graph(weighted=True)
+
+        # dodajemy wszystkie wierzcholki
+        for vertex_id in vertices:
+            graph.add_vertex(Vertex(vertex_id))
+
+        # dodajemy krawedzie z naszych kombinacji
+        graph.add_edges(edges)
+
+        # powtarzamy proces dopoki graf nie bedzie spojny
+        if graph.is_consistent():
+            return graph
+
+
+def draw(graph: Graph, filename: str = None):
+    """
+    Funkcja odpowiada za narysowanie grafu
+    :param graph: graf do narysowania
+    :param filename: opcjonalna nazwa pliku do zapisu w nim rysunku
+    """
+    # pusty graf
+    nx_graph = nx.Graph()
+
+    # dodanie wierzcholkow do grafu
+    for vertex in graph.get_vertices():
+        nx_graph.add_node(vertex.get_id())
+
+    edges = graph.get_edges()
+
+    # dodanie krawedzi do grafu
+    for edge in edges:
+        vertices_ids = edge.get_vertices_ids()
+        if graph.is_weighted():
+            nx_graph.add_edge(vertices_ids[0].get_id(), vertices_ids[1].get_id(), weight=edge.get_weight())
+        else:
+            nx_graph.add_edge(vertices_ids[0].get_id(), vertices_ids[1].get_id())
+
+    # ustawienie numerow wierzcholkow
+    labels = {}
+    for i in range(len(graph.get_vertices())):
+        labels[i] = i + 1
+
+    # pozycjonowanie na okregu
+    pos = nx.circular_layout(nx_graph)
+
+    # narysowanie grafu
+    nx.draw(nx_graph, pos, labels=labels)
+
+    # dodanie wag krawedzi do rysunku
+    if graph.is_weighted():
+        nx.draw_networkx_edge_labels(nx_graph, pos, edge_labels=nx.get_edge_attributes(nx_graph, "weight"))
+
+    # dla symetrycznosci
+    plt.axis("equal")
+
+    # ewentualny zapis do pliku
+    if filename is not None:
+        plt.savefig(filename, format="png", bbox_inches='tight')
+    plt.show()
+
+    # wyczyszczenie figury
+    plt.clf()
+
+
+# zadanie 1
+graph = create_rand_consistent_weighted_graph(5, 1, 10)
+draw(graph, 'spojny_losowy_graf_wazony.png')
---
+++ structures/Graph.py
@@ -0,0 +1,103 @@
+from typing import List, Set
+from labs.structures.Edge import Edge
+from labs.structures.Vertex import Vertex
+
+
+class Graph:
+    """
+       Klasa reprezentujaca Graf
+    """
+    edges: List[Edge]
+    vertices: Set[Vertex]
+    weighted: bool
+
+    def __init__(self, edges: List[Edge] = None, vertices: Set[Vertex] = None, weighted=False):
+        """
+        Inicjalizacja grafu
+        :param edges: krawedzie
+        :param vertices: wezly
+        :param weighted: czy krawedzie maja wagi
+        """
+        if edges is None:
+            edges = []
+        if vertices is None:
+            vertices = set()
+        self.edges = edges
+        self.vertices = vertices
+        self.weighted = weighted
+
+    def add_vertex(self, vertex: Vertex):
+        """
+        Dodaje nowy wezel
+        :param vertex: wezel
+        """
+        self.vertices.add(vertex)
+
+    def add_edges(self, edges: Set[tuple]):
+            """
+            Dodaje krawedzie
+            :param edges: zbior tupli wierzcholkow lub wierzcholkow i wagi
+            """
+            for edge in edges:
+                # self.vertices.add(edge[0])
+                # self.vertices.add(edge[1])
+
+                # tutaj dodajemy krawedzie do grafu
+                has_edge = None
+                for x in self.edges:
+                    # jesli krawedz juz zostala dodana do grafu to wychodzimy z wewnetrznej petli i
+                    # sprawdzamy kolejna krawedz ze zbioru krawedzi ktore chcemy dodac
+                    if (x.get_vertices_ids()[0] == edge[1] and x.get_vertices_ids()[1] == edge[0]) or (
+                            x.get_vertices_ids()[0] == edge[0] and x.get_vertices_ids()[1] == edge[1]):
+                        has_edge = x
+                        break
+                # jesli nie to dodajemy brakujaca krawedz
+                if has_edge is None:
+                    if self.weighted:
+                        self.edges.append(Edge(edge[0], edge[1], edge[2]))
+                    else:
+                        self.edges.append(Edge(edge[0], edge[1]))
+
+    def get_edges(self) -> List[Edge]:
+        """
+        Zwraca liste krawedzi
+        """
+        return self.edges
+
+    def get_vertices(self) -> Set[Vertex]:
+        """
+        Zwraca zbior wezlow
+        """
+        return self.vertices
+
+    def get_vertices_lvls(self) -> List[int]:
+        """
+        Zwraca liste stopni wierzcholkow
+        """
+        nr_of_vertices = len(self.get_vertices())
+        vertex_level = [0] * nr_of_vertices
+        edges = self.get_edges()
+        for edge in edges:
+            edge_vertices = edge.get_vertices_ids()
+            # zwiekszamy stopien danego wierzcholka o jeden dla kazdej krawedzi
+            # i zapisujemy inkrementowana wartosc stopnia wierzcholka pod danym id wierzcholka w tablicy
+            vertex_level[edge_vertices[0].get_id()] += 1
+            vertex_level[edge_vertices[1].get_id()] += 1
+        return vertex_level
+
+    def is_consistent(self) -> bool:
+        """
+        Sprawdza czy graf jest spojny
+        :return: prawda/falsz
+        """
+        vertices_lvls = self.get_vertices_lvls()
+        nr_of_zero_lvls = len(list(filter(lambda lvl: lvl == 0, vertices_lvls)))
+        # jesli istnieje wierzcholek ktorego stopien == 0, to graf na pewno nie jest spojny
+        return nr_of_zero_lvls == 0
+
+    def is_weighted(self) -> bool:
+        """
+        Zwraca informacje czy krawedzie grafu maja wagi
+        :return: prawda/falsz
+        """
+        return self.weighted
