bf5cfe9c4de4d9d9728744f0869cb33c5c9cc3cb
radzikkos <lukaszrad99@wp.pl>
2021-05-18 00:09:45 +0200

    Kosaraju should work correctly

--- lab4/kosaraju.py
+++ lab4/kosaraju.py
@@ -1,44 +1,66 @@
 import ex1
 import numpy as np
-import random
 
-"""
-Nie dziala dla pustych list sasiedztwa
-"""
 def DFS_Visit(v, G, d, f, t):
+    """
+    DFS algorithm
+    :param v: vertex of graph
+    :param G: graph in list of neighbours
+    :param d: list of visiting times
+    :param f: list of going through vertex
+    :param t: time
+    :return: t
+    """
     t = t + 1
     d[v] = t
-    #print("BERORE" + str(t))
 
     for neighbour_v in G[v]:
         if d[neighbour_v] == -1:
             t = DFS_Visit(neighbour_v, G, d, f, t)
     t = t + 1
     f[v] = t
-    #print("AFTER"+ str(t))
+
     return t
 
 
 def components_R(nr, v, G_Trans, comp):
-    try:
-        if G_Trans[v] == []:
-            print("KURwa")
-        for neighbour_v in G_Trans[v]:
-            if comp[neighbour_v] == -1:
-                comp[neighbour_v] = nr
-                components_R(nr,neighbour_v, G_Trans, comp)
-    except:
-        print("COMPONENT ERROR")
+    """
+
+    :param nr: number of cluster
+    :param v: vertex
+    :param G_Trans: reverse matrix to entry matrix
+    :param comp: array of clusters
+    :return:
+    """
+    if v not in G_Trans:
+        nr = nr + 1
+        comp[v] = nr
+        return
+    for neighbour_v in G_Trans[v]:
+        if neighbour_v not in comp:
+            # nr = nr + 1
+            # print(neighbour_v)
+            # comp[neighbour_v] = nr
+            # print(comp)
+            # components_R(nr, neighbour_v, G_Trans, comp)
+            print()
+        elif comp[neighbour_v] == -1:
+            # nr = nr + 1
+            comp[neighbour_v] = nr
+            components_R(nr,neighbour_v, G_Trans, comp)
 
 
 def revert_matrix_values(list_neighbours):
-    #print(matrix)
-    #list_from_matrix = ex1.from_matrix_neighbour_to_list(matrix)
+    """
+    Function returns reversed graph
+    :param list_neighbours: graph in list of neighbours
+    :return: list of neighbours but reversed to the entry list
+    """
     incidence_matrix = ex1.from_list_to_incidence_matrix(list_neighbours)
     shape = np.shape(incidence_matrix)
     rows = 0
     columns = 1
-    #print(incidence_matrix)
+
     for row in range(shape[rows]):
         for column in range(shape[columns]):
             if incidence_matrix[row,column] == 1:
@@ -46,13 +68,36 @@ def revert_matrix_values(list_neighbours):
             elif incidence_matrix[row,column] == -1:
                 incidence_matrix[row,column] = 1
     return ex1.from_incidence_matrix_to_list(incidence_matrix)
-    # for row in incidence_matrix:
-    #     print(len(row[0]))
 
-    #print(incidence_matrix)
+def sort_f(f):
+    """
+    Funcion helps to sort times
+    :param f:
+    :return:
+    """
+    return dict(sorted(f.items(), key=lambda item: item[1], reverse=True)).keys()
 
+def sort_G_by_f(G, f):
+    """
+    Function sorts Graph using times
+    :param G:
+    :param f:
+    :return:
+    """
+    new_G = {}
+    for element in f:
+        try:
+            new_G[element] = G[element]
+        except:
+            new_G[element] = []
+    return new_G
 
 def kosaraju(G):
+    """
+    Kosaraju algorithm
+    :param G: grapgh represented as list of neighbours
+    :return: dictionary - vertex and cluster where vertex fits
+    """
     d = {}
     f = {}
     for v in G.keys():
@@ -66,43 +111,66 @@ def kosaraju(G):
     nr = 0
 
     comp = {}
-    #print(len(G))
+
     for v in G_trans.keys():
         comp[v] = -1
-    #print(comp)
-    #print(G)
-    #print(G_trans)
-    #print(f)
-    # print(dict( sorted(f.items(),key=lambda item: item[1],reverse=True)))
-    # print(comp)
-    edges = G_trans
-    # print(edges)
-    for v in dict( sorted(f.items(),key=lambda item: item[1],reverse=True)).keys():
-        # print(v)
-        #print(comp[v])
-        try:
-            if comp[v] == -1:
-                nr = nr + 1
-                comp[v] = nr
-                components_R(nr,v, G_trans, comp)
-        except:
-            print("KOsaraju error")
-    #print(f)
-    #print(comp)
+
+    for v in sort_G_by_f(G_trans, sort_f(f)).keys():
+
+        if v not in comp:
+            nr = nr + 1
+            comp[v] = nr
+
+        elif comp[v] == -1:
+            nr = nr + 1
+            comp[v] = nr
+            components_R(nr, v, G_trans, comp)
+
     return comp
 
+def group_result(input):
+   """
+   Functions groups the results of kosaraju function to clusters which are represented by dictonary and list
+   :param input:  result from kosaraju function
+   :return: dictionaty with key - number of cluster , value - array of vertexes which fit to cluster
+   """
+   results = dict(sorted(input.items(), key=lambda item: item[1]))
+   list_of_results = {}
 
-# n = 10
-# p = 0.5
-# matrix = ex1.rand_digraph_with_probability(n,p)
-# list_matrix = ex1.from_matrix_neighbour_to_list(matrix)
+   for key, value  in results.items():
+       if value not in list_of_results:
+          temp = []
+          temp.append(key)
+          list_of_results[value] = temp
+       else:
+           list_of_results[value].append(key)
+
+   return list_of_results
+
+n = 15
+p = 0.1
+matrix = ex1.rand_digraph_with_probability(n,p)
+list_matrix = ex1.from_matrix_neighbour_to_list(matrix)
 #
 # revert_matrix_values(list_matrix)
 #
-# result = kosaraju(list_matrix)
-# print(list_matrix)
-# print(result)
+result = kosaraju(list_matrix)
+print(list_matrix)
+print(result)
+print(group_result(result))
 
+# list_my = {
+#           1 : [2],
+#           2 : [1],
+#           3 : [5],
+#           4 : [5],
+#             5: [3],
+# }
+# res= kosaraju(list_my)
+# print()
+# print(list_my)
+# print(res)
+# print(group_result(res))
 
 
 
